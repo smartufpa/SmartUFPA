f500f173d39e0f7edcfb636e3df3addce1e2005b
kaeuchoa <kaeuchoa@hotmail.com>
2016-09-30 11:43:48 -0300

    Adiciona o ícone de localização do usuário e o tracking. Além da checagem de permissões para dispositivos com Android Marshmallow+

--- app/src/main/java/com/example/kaeuc/osmapp/MainActivity.java
+++ app/src/main/java/com/example/kaeuc/osmapp/MainActivity.java
@@ -1,30 +1,56 @@
 package com.example.kaeuc.osmapp;
 
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
     MapView mMap;
     IMapController mMapController;
     Local defaultLocation;
     MyLocationNewOverlay mLocationOverlay;
+
+    private Location myCurrentLocation;
+    private LocationManager locationManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Views
-        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relative_layout);
-        this.mMap = new MapView(this);
-        rl.addView(mMap, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT,RelativeLayout.LayoutParams.FILL_PARENT));
+        // Requisita permissões para Android Marshmallow e devices superiores
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
 
 
         /* Configura o caminho do armazenamento em cache do mapa, se o device não possui cartão SD,
@@ -37,38 +63,157 @@ public class MainActivity extends AppCompatActivity {
          * Um exemplo mostra a utilização de "BuildConfig.APPLICATION_ID"
          */
         org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+        // Views
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relative_layout);
+        this.mMap = new MapView(this);
+        rl.addView(mMap, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT,RelativeLayout.LayoutParams.FILL_PARENT));
 
 
-        // Configuração do Mapa
-        this.mMap.setTilesScaledToDpi(true);
-        this.mMap.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-        this.mMap.setBuiltInZoomControls(true);
-        this.mMap.setMinZoomLevel(15);
-        this.mMap.setMultiTouchControls(true);
-        mMap.setUseDataConnection(true);
+        mLocationOverlay = new MyLocationNewOverlay(this,new GpsMyLocationProvider(this),mMap);
 
         // Configuração do MapController: Posição inicial e zoom
         defaultLocation = new Local(-1.47485, -48.45651,"UFPA");
         GeoPoint startPoint = new GeoPoint(defaultLocation.getLatitude(),defaultLocation.getLongitude());
-        this.mMapController = this.mMap.getController();
-        this.mMapController.setZoom(16);
-        this.mMapController.animateTo(startPoint);
+        mMapController = this.mMap.getController();
+        mMapController.setZoom(16);
+        mMapController.animateTo(startPoint);
 
-        // Configuração do MyLocationOverlay
+        // Configuração do Mapa
+        mMap.setTilesScaledToDpi(true);
+        mMap.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        mMap.setBuiltInZoomControls(true);
+        mMap.setMinZoomLevel(15);
+        mMap.setMultiTouchControls(true);
+        mMap.setUseDataConnection(true);
+        mMap.getOverlays().add(this.mLocationOverlay);
 
-        mLocationOverlay = new MyLocationNewOverlay(getBaseContext(),this.mMap);
+        // Configuração para mostrar o boneco da posição do usuário
         mLocationOverlay.enableMyLocation();
-        this.mMap.getOverlays().add(mLocationOverlay);
-
-
-
-
-
-
-
-
+        mLocationOverlay.enableFollowLocation();
+        mLocationOverlay.setOptionsMenuEnabled(true);
 
 
 
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(this);
+        }
+        mLocationOverlay.disableFollowLocation();
+        mLocationOverlay.disableMyLocation();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0l,0f,this);
+        }
+
+        mLocationOverlay.enableFollowLocation();
+        mLocationOverlay.enableMyLocation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationManager = null;
+        myCurrentLocation=null;
+        mLocationOverlay=null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        myCurrentLocation = location;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    // Inicio da checagem de permissões - Métodos retirados do exemplo dado no projeto do OSMdroid
+    // OpenStreetMapView > Samples > ExtraSamples > SampleFollowMe
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "osmdroid permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nLocation to show user location.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                if (location && storage) {
+                    // All Permissions Granted
+                    Toast.makeText(MainActivity.this, "Todas as permissões necessárias foram cedidas.",
+                            Toast.LENGTH_SHORT).show();
+                } else if (location) {
+                    Toast.makeText(this, "Permissão de armzenamento é necessária para armazenar " +
+                            "porções do mapa para reduzir consumo de dados e para uso offline.",
+                            Toast.LENGTH_LONG).show();
+                } else if (storage) {
+                    Toast.makeText(this, "Permissão de localização é necessária para mostrar a sua " +
+                            "localização no mapa.", Toast.LENGTH_LONG).show();
+                } else { // !location && !storage case
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "Permissão de armzenamento é necessária para armazenar " +
+                            "porções do mapa para reduzir consumo de dados e para uso offline." +
+                            "\nPermissão de localização é necessária para mostrar a sua " +
+                            "localização no mapa.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    // END PERMISSION CHEC
+
+
+
+
+
+
 }
