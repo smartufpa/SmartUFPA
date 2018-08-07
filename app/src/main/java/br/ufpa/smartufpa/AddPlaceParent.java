a219fd7638cbe38bb42de6e1d84b1d43d76b9ce3
kaeuchoa <kaeuchoas@gmail.com>
2018-08-06 21:10:49 -0300

    refactor: adiciona o objeto JSON à AddPlaceParent

--- app/src/main/java/br/ufpa/smartufpa/AddPlaceParent.java
+++ app/src/main/java/br/ufpa/smartufpa/AddPlaceParent.java
@@ -2,6 +2,11 @@ package br.ufpa.smartufpa;
 
 import android.support.v7.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
 public class AddPlaceParent extends AppCompatActivity {
 
     protected static final String ARG_LATITUDE = "latitude";
@@ -10,5 +15,14 @@ public class AddPlaceParent extends AppCompatActivity {
     protected double latitude;
     protected double longitude;
 
+    protected JSONObject json;
 
+    public AddPlaceParent() {
+        this.json = new JSONObject();
+    }
+
+
+    public JSONObject getJson() {
+        return json;
+    }
 }
