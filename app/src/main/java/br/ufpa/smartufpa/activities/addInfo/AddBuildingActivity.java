d5205d53095302e8dcfc90a0fae232aa5381af11
kaeuchoa <kaeuchoa@hotmail.com>
2018-09-05 11:27:38 -0300

    chore: atualiza os arquivos do gradle e retira entradas desnecessarias

---
+++ app/src/main/java/br/ufpa/smartufpa/activities/addInfo/AddBuildingActivity.java
@@ -0,0 +1,13 @@
+package br.ufpa.smartufpa.activities.addInfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddBuildingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_building);
+    }
+}
